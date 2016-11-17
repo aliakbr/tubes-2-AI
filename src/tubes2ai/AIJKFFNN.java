fb75d525e9779d8f5e5502568a8aa2a6506f7d59
Isham Faizal <i@adimaja.com>
2016-11-17 14:41:39 +0700

    Add capability and option handling

--- src/tubes2ai/AIJKFFNN.java
+++ src/tubes2ai/AIJKFFNN.java
@@ -5,16 +5,24 @@
  */
 package tubes2ai;
 
+import weka.associations.NominalItem;
 import weka.classifiers.Classifier;
 import weka.core.*;
+import weka.core.Capabilities.Capability;
 
-import java.util.Enumeration;
+import java.io.Serializable;
+import java.util.*;
 
 /**
  *
  * @author i
  */
-public class AIJKFFNN implements Classifier, OptionHandler {
+public class AIJKFFNN implements Classifier, OptionHandler, CapabilitiesHandler, Serializable {
+
+    public AIJKFFNN() {
+        hiddenLayerCount = 0;
+        hiddenLayerNeuronCount = 0;
+    }
 
     @Override
     public void buildClassifier(Instances instances) throws Exception {
@@ -33,21 +41,33 @@ public class AIJKFFNN implements Classifier, OptionHandler {
 
     @Override
     public Capabilities getCapabilities() {
-        return null;
+        Capabilities c = new Capabilities(this);
+        c.enable(Capability.NUMERIC_ATTRIBUTES);
+        c.enable(Capability.NOMINAL_CLASS);
+        return c;
     }
 
     @Override
     public Enumeration<Option> listOptions() {
-        return null;
+        Vector<Option> options = new Vector<>();
+
+        options.add(new Option("Amount of hidden layers", "L", 1, "-L <amount>"));
+        options.add(new Option("Amount of neurons in hidden layer", "N", 1, "-N <amount>"));
+
+        return options.elements();
     }
 
     @Override
     public void setOptions(String[] strings) throws Exception {
-
+        hiddenLayerCount = Integer.parseInt(Utils.getOption("L", strings));
+        hiddenLayerNeuronCount = Integer.parseInt(Utils.getOption("N", strings));
     }
 
     @Override
     public String[] getOptions() {
-        return new String[0];
+        return new String[]{"-L", String.valueOf(hiddenLayerCount), "-N", String.valueOf(hiddenLayerNeuronCount)};
     }
+
+    private int hiddenLayerCount;
+    private int hiddenLayerNeuronCount;
 }
