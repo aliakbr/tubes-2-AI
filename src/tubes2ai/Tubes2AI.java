1c3f65444e5e2f3d1c9b38f1f180179db9d0503f
Isham Faizal <i@adimaja.com>
2016-11-19 13:53:00 +0700

    Add code for testing with iris.arff

--- src/tubes2ai/Tubes2AI.java
+++ src/tubes2ai/Tubes2AI.java
@@ -5,6 +5,14 @@
  */
 package tubes2ai;
 
+import weka.classifiers.Classifier;
+import weka.classifiers.Evaluation;
+import weka.core.Instances;
+import weka.core.Utils;
+import weka.core.converters.ConverterUtils;
+
+import java.util.Random;
+
 /**
  *
  * @author i
@@ -15,7 +23,27 @@ public class Tubes2AI {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ConverterUtils.DataSource source;
+
+        try {
+            AIJKFFNN classifier = new AIJKFFNN();
+            classifier.setOptions(Utils.splitOptions(""));
+            source = new ConverterUtils.DataSource("data/iris.arff");
+            Instances data = source.getDataSet();
+            data.setClassIndex(data.numAttributes() - 1);
+
+            Evaluation eval10fold = new Evaluation(data);
+            eval10fold.crossValidateModel(
+                classifier,
+                data,
+                10,
+                new Random(1)
+            );
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
     
 }
